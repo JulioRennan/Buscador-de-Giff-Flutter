c3d43b27a4153fac8b5ae49e020552f8801b65e2
JulioRennan <57741609+JulioRennan@users.noreply.github.com>
2020-08-04 14:44:20 -0300

    Codigo
    Codigo.

---
+++ android/app/src/main/kotlin/com/example/buscadorgiffsflutterudemy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.buscadorgiffsflutterudemy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:buscadorgiffsflutterudemy/ui/home_page.dart';
+import 'package:buscadorgiffsflutterudemy/ui/gif_page.dart';
+import 'package:http/http.dart' as http;
+void main(){
+  runApp(MaterialApp(
+        home: HomePage(),
+        theme: ThemeData.light(),
+  ));
+
+}
